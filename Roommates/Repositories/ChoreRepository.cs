e46b032b8bd352b42f5116013b1a952850d0ab77
Thomas Witherow <twitherow@bellsouth.net>
2021-09-13 12:02:29 -0500

    chores are here

--- Roommates/Repositories/ChoreRepository.cs
+++ Roommates/Repositories/ChoreRepository.cs
@@ -1,128 +1,128 @@
-//using Microsoft.Data.SqlClient;
-//using Roommates.Models;
-//using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using Roommates.Models;
+using System.Collections.Generic;
 
-//namespace Roommates.Repositories
-//{
-//    public class ChoreRepository : BaseRepository
+namespace Roommates.Repositories
+{
+    public class ChoreRepository : BaseRepository
    
-//    {
-//        public ChoreRepository(string connectionString) : base(connectionString) { }
+    {
+        public ChoreRepository(string connectionString) : base(connectionString) { }
 
-//        public List<Chore> GetAll()
-//        {
-//            //  We must "use" the database connection.
-//            //  Because a database is a shared resource (other applications may be using it too) we must
-//            //  be careful about how we interact with it. Specifically, we Open() connections when we need to
-//            //  interact with the database and we Close() them when we're finished.
-//            //  In C#, a "using" block ensures we correctly disconnect from a resource even if there is an error.
-//            //  For database connections, this means the connection will be properly closed.
-//            using (SqlConnection conn = Connection)
-//            {
-//                // Note, we must Open() the connection, the "using" block doesn't do that for us.
-//                conn.Open();
+        public List<Chore> GetAll()
+        {
+            //  We must "use" the database connection.
+            //  Because a database is a shared resource (other applications may be using it too) we must
+            //  be careful about how we interact with it. Specifically, we Open() connections when we need to
+            //  interact with the database and we Close() them when we're finished.
+            //  In C#, a "using" block ensures we correctly disconnect from a resource even if there is an error.
+            //  For database connections, this means the connection will be properly closed.
+            using (SqlConnection conn = Connection)
+            {
+                // Note, we must Open() the connection, the "using" block doesn't do that for us.
+                conn.Open();
 
-//                // We must "use" commands too.
-//                using (SqlCommand cmd = conn.CreateCommand())
-//                {
-//                    // Here we setup the command with the SQL we want to execute before we execute it.
-//                    cmd.CommandText = "SELECT Id, Name From Chore";
+                // We must "use" commands too.
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    // Here we setup the command with the SQL we want to execute before we execute it.
+                    cmd.CommandText = "SELECT Id, Name From Chore";
 
-//                    // Execute the SQL in the database and get a "reader" that will give us access to the data.
-//                    SqlDataReader reader = cmd.ExecuteReader();
+                    // Execute the SQL in the database and get a "reader" that will give us access to the data.
+                    SqlDataReader reader = cmd.ExecuteReader();
 
-//                    // A list to hold the rooms we retrieve from the database.
-//                    List<Chore> chores = new List<Chore>();
+                    // A list to hold the rooms we retrieve from the database.
+                    List<Chore> chores = new List<Chore>();
 
-//                    // Read() will return true if there's more data to read
-//                    while (reader.Read())
-//                    {
-//                        // The "ordinal" is the numeric position of the column in the query results.
-//                        //  For our query, "Id" has an ordinal value of 0 and "Name" is 1.
-//                        int idColumnPosition = reader.GetOrdinal("Id");
+                    // Read() will return true if there's more data to read
+                    while (reader.Read())
+                    {
+                        // The "ordinal" is the numeric position of the column in the query results.
+                        //  For our query, "Id" has an ordinal value of 0 and "Name" is 1.
+                        int idColumnPosition = reader.GetOrdinal("Id");
 
-//                        // We user the reader's GetXXX methods to get the value for a particular ordinal.
-//                        int idValue = reader.GetInt32(idColumnPosition);
+                        // We user the reader's GetXXX methods to get the value for a particular ordinal.
+                        int idValue = reader.GetInt32(idColumnPosition);
 
-//                        int nameColumnPosition = reader.GetOrdinal("Name");
-//                        string nameValue = reader.GetString(nameColumnPosition);
+                        int nameColumnPosition = reader.GetOrdinal("Name");
+                        string nameValue = reader.GetString(nameColumnPosition);
 
 
-//                        // Now let's create a new Chore object using the data from the database.
-//                        Chore chore = new Chore
-//                        {
-//                            Id = idValue,
-//                            Name = nameValue
-//                        };
-//                        // ...and add that chore object to our list.
-//                        chores.Add(chore);
-//                    }
+                        // Now let's create a new Chore object using the data from the database.
+                        Chore chore = new Chore
+                        {
+                            Id = idValue,
+                            Name = nameValue,
+                        };
+                        // ...and add that chore object to our list.
+                        chores.Add(chore);
+                    }
 
-//                    // We should Close() the reader. Unfortunately, a "using" block won't work here.
-//                    reader.Close();
+                    // We should Close() the reader. Unfortunately, a "using" block won't work here.
+                    reader.Close();
 
-//                    // Return the list of rooms who whomever called this method.
-//                    return chores;
-//                }
-//            }
-//        }
+                    // Return the list of rooms who whomever called this method.
+                    return chores;
+                }
+            }
+        }
 
-//        /// <summary>
-//        ///  Returns a single room with the given id.
-//        /// </summary>
+        /// <summary>
+        ///  Returns a single room with the given id.
+        /// </summary>
 
-//        public Chore GetById(int Id)
-//        {
-//            using (SqlConnection conn = Connection)
-//            {
-//                conn.Open();
-//                using (SqlCommand cmd = conn.CreateCommand())
-//                {
-//                    cmd.CommandText = "Select Name FROM Chore WHERE Id = @Id";
-//                    cmd.Parameters.AddWithValue("@id", id);
-//                    SqlDataReader reader = cmd.ExecuteReader();
+        public Chore GetById(int choreId)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT Name FROM Chore WHERE Id = @id";
+                    cmd.Parameters.AddWithValue("@id", choreId);
+                    SqlDataReader reader = cmd.ExecuteReader();
 
-//                    Chore chore = null;
+                    Chore chore = null;
 
-//                    if (reader.Read())
-//                    {
-//                        chore = new Chore
-//                        {
-//                            Id = Id,
-//                            Name = reader.GetString(reader.GetOrdinal("Name"))
-//                        };
-//                    }
+                    if (reader.Read())
+                    {
+                        chore = new Chore
+                        {
+                            Id = choreId,
+                            Name = reader.GetString(reader.GetOrdinal("Name"))
+                        };
+                    }
 
-//                    reader.Close();
+                    reader.Close();
 
-//                    return chore;
+                    return chore;
 
-//                }
-//            }
-//        }
+                }
+            }
+        }
 
-//        /// <summary>
-//        ///  Add a new room to the database
-//        ///   NOTE: This method sends data to the database,
-//        ///   it does not get anything from the database, so there is nothing to return.
-//        /// </summary>
+        /// <summary>
+        ///  Add a new room to the database
+        ///   NOTE: This method sends data to the database,
+        ///   it does not get anything from the database, so there is nothing to return.
+        /// </summary>
 
-//        public void Insert(Chore chore)
-//        {
-//            using (SqlConnection conn = Connection)
-//            {
-//                conn.Open();
-//                using (SqlCommand cmd = conn.CreateCommand())
-//                {
-//                    cmd.CommandText = @"INSERT INTO Room (Name)
-//                                        OUTPUT INSERTED.Id
-//                                        VALUES (@name)";
-//                    cmd.Parameters.AddWithValue("@name", chore.Name);
-//                    int id = (int)cmd.ExecuteScalar();
+        public void Insert(Chore chore)
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"INSERT INTO Room (Name)
+                                        OUTPUT INSERTED.Id
+                                        VALUES (@name)";
+                    cmd.Parameters.AddWithValue("@name", chore.Name);
+                    int id = (int)cmd.ExecuteScalar();
 
-//                    chore.Id = id;
-//                }
-//            }
-//        }
-//    }
-//}
+                    chore.Id = id;
+                }
+            }
+        }
+    }
+}
